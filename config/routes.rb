a48b85024b132c2a1ebda466d06218cef4c9aa56
Shakhram <shakag6@gmail.com>
2023-12-08 21:35:48 -0500

    Clean up get routes by replacing it with 'resource'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "items#index"
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
+  # get "/items", to: "items#index"
+  # get "/items/:id", to: "items#show"
+  # Rails provides a routes method named resources that maps all of the conventional routes for a collection of resources, such as items
+  resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
