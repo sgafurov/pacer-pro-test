2935491982d23d90575ed412aab1ab72ced0420f
Shakhram <shakag6@gmail.com>
2023-12-08 21:55:59 -0500

    Add methods to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+    def soft_delete
+        #updates the deleted_at attribute with the current timestamp.
+        update(deleted_at: Time.current)
+
+    def soft_delete
+        #sets the deleted_at attribute to nil
+        update(deleted_at: nil)
+
+    default_scope where(:deleted_at => nil)
 end
