8eab5f34714451f03d873b48c04a57db821fdf52
Shakhram <shakag6@gmail.com>
2023-12-08 23:28:38 -0500

    Write tests for Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,12 @@ class Item < ApplicationRecord
     def soft_delete
         #updates the deleted_at attribute with the current timestamp.
         update(deleted_at: Time.current)
+    end
 
-    def soft_delete
+    def restore
         #sets the deleted_at attribute to nil
         update(deleted_at: nil)
+    end
 
-    default_scope where(:deleted_at => nil)
+    default_scope {where(:deleted_at => nil)}
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+    it "Soft deletes item" do
+        item = Item.create(name:"", deleted_at:nil)
+        item.soft_delete
+        expect(item.deleted_at).not_to be_nil
+    end
+
+    it "Restores soft deleted item" do
+        item = Item.create(name:"", deleted_at:nil)
+        item.soft_delete
+        item.restore
+        expect(item.deleted_at).to be_nil
+    end
+
+    it "Excludes soft deleted items from normal queries" do
+        normalItem = Item.create(name:"", deleted_at:nil)
+        softDeletedItem = Item.create(name:"", deleted_at:nil)
+        softDeletedItem.soft_delete
+        expect(Item.all).to include(normalItem)
+        expect(Item.all).not_to include(softDeletedItem)
+    end
+
+end
